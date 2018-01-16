bdf7b4c54fa51da7df99454647ff35facdba4f24
marcio araujo <maritsu2015@gmail.com>
2018-01-16 16:41:11 -0200

    dynamic inclusion of servers in the list

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,8 +10,9 @@ export class ServersComponent implements OnInit {
   
   allowNewServer = false;
   serverCreationStatus = "No server was created";
-  serverName = " empty";
+  serverName = "";
   serverCreated = false;
+  servers = ['testServer', 'testServer2']
 
   constructor() { 
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
 
